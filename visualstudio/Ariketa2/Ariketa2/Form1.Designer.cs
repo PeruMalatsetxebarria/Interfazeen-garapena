110252e165a3af4f48fefe7de992bb2dbfe87853
peruMB <113597099+peruMB@users.noreply.github.com>
2022-09-29 11:00:48 +0200

    Add files via upload

---
+++ visualstudio/Ariketa2/Ariketa2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Ariketa2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testua = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Hurrengoa = new System.Windows.Forms.Button();
+            this.Garbitu = new System.Windows.Forms.Button();
+            this.Irten = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testua
+            // 
+            this.testua.AutoSize = true;
+            this.testua.Location = new System.Drawing.Point(167, 65);
+            this.testua.Name = "testua";
+            this.testua.Size = new System.Drawing.Size(38, 15);
+            this.testua.TabIndex = 0;
+            this.testua.Text = "label1";
+            this.testua.Click += new System.EventHandler(this.testua_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(378, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Hurrengoa
+            // 
+            this.Hurrengoa.Location = new System.Drawing.Point(176, 156);
+            this.Hurrengoa.Name = "Hurrengoa";
+            this.Hurrengoa.Size = new System.Drawing.Size(75, 23);
+            this.Hurrengoa.TabIndex = 2;
+            this.Hurrengoa.Text = "Hurrengoa";
+            this.Hurrengoa.UseVisualStyleBackColor = true;
+            this.Hurrengoa.Click += new System.EventHandler(this.Hurrengoa_Click);
+            // 
+            // Garbitu
+            // 
+            this.Garbitu.Location = new System.Drawing.Point(337, 165);
+            this.Garbitu.Name = "Garbitu";
+            this.Garbitu.Size = new System.Drawing.Size(75, 23);
+            this.Garbitu.TabIndex = 3;
+            this.Garbitu.Text = "Garbitu";
+            this.Garbitu.UseVisualStyleBackColor = true;
+            this.Garbitu.Click += new System.EventHandler(this.Garbitu_Click);
+            // 
+            // Irten
+            // 
+            this.Irten.Location = new System.Drawing.Point(509, 179);
+            this.Irten.Name = "Irten";
+            this.Irten.Size = new System.Drawing.Size(75, 23);
+            this.Irten.TabIndex = 4;
+            this.Irten.Text = "Irten";
+            this.Irten.UseVisualStyleBackColor = true;
+            this.Irten.Click += new System.EventHandler(this.Irten_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Irten);
+            this.Controls.Add(this.Garbitu);
+            this.Controls.Add(this.Hurrengoa);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.testua);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label testua;
+        private TextBox textBox1;
+        private Button Hurrengoa;
+        private Button Garbitu;
+        private Button Irten;
+    }
+}
---
+++ visualstudio/Ariketa2/Ariketa2/Form1.cs
@@ -0,0 +1,49 @@
+namespace Ariketa2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //formuli: (a+2b+3c+4d)/4
+
+        int a = 3;
+        int b = 3;
+        int c = 3;
+        int d = 3;
+        const int e = 2;
+        const int f = 3;
+        const int g = 4;
+
+        int balioa = (a + (e * b) + (c * f) + (d * g)) /g;
+
+        testua.Text;
+
+        private void testua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hurrengoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Garbitu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Irten_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
