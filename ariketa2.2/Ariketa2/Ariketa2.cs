22c6333807e0aa6a6eaa40fe14b13f711b8ee587
peruMB <113597099+peruMB@users.noreply.github.com>
2022-09-30 14:26:23 +0200

    Add files via upload

---
+++ ariketa2.2/Ariketa2/Ariketa2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariketa2
+{
+    internal class Ariketa2
+    {
+        public string Label { get; set; }
+        public double Zenbakia { get; set; }
+    }
+}
---
+++ ariketa2.2/Ariketa2/Zenbakiak.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariketa2
+{
+    public class Zenbakiak
+    {
+        public float a { get; set; }
+        public float b { get; set; }
+        public float c { get; set; }
+        public float d { get; set; }
+
+        public Zenbakiak()
+        {
+
+        }
+        public float eragiketa()
+        {
+            float emaitza = 0;
+            emaitza = (a + (2 * b) + (3 * c) + (4 * d)) / 4;
+            return emaitza;
+        }
+    }
+}
